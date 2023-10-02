5076e1fade7142d3e954b148f5017e57de1e4c9f
marcelovidalp <marcelo.vidal2023@alu.uct.cl>
2023-10-02 10:51:59 -0300

    Update S_Animales.py

---
+++ S_Animales.py
@@ -0,0 +1,38 @@
+class Animal:
+    def __init__(self, nombre, edad):
+        self.nombre = nombre 
+        self.edad = edad
+
+class Perro(Animal):
+    def __init__(self, nombre, edad):
+        super().__init__(nombre, edad)
+        self.sonido = "¡GUAU GUAU!"
+
+    def hacerSonido(self):
+        return f"{self.nombre} con {self.edad} de edad, esta hdiciendo {self.sonido}"
+    
+class Pajaro(Animal):
+    def __init__(self, nombre, edad):
+        super().__init__(nombre, edad)
+        self.sonido = "¡PIO PIO!"
+    
+    def hacerSonido(self):
+        return f"{self.nombre} con {self.edad} de edad, esta diciendo {self.sonido}"
+        
+
+class Gato(Animal):
+    def __init__(self, nombre, edad):
+        super().__init__(nombre, edad)
+        self.sonido = "¡Miauu!"
+
+    def hacerSonido(self):
+        return f"{self.nombre} con {self.edad} de edad, esta diciendo {self.sonido}"
+    
+animal1 = Perro("Tomy", 5)
+print(animal1.hacerSonido())
+
+animal2 = Gato("Kite", 2)
+print(animal2.hacerSonido())
+
+animal3 = Pajaro("Piolin", 2)
+print(animal3.hacerSonido())
