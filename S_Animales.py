0952f64fffcdf7af8a650fdcd9c0161691409046
marcelovidalp <marcelo.vidal2023@alu.uct.cl>
2023-10-02 11:29:19 -0300

    Update S_Animales.py

--- S_Animales.py
+++ S_Animales.py
@@ -4,35 +4,35 @@ class Animal:
         self.edad = edad
 
 class Perro(Animal):
-    def __init__(self, nombre, edad):
+    def __init__(self, nombre, edad, sonido):
         super().__init__(nombre, edad)
-        self.sonido = "¡GUAU GUAU!"
+        self.sonido = sonido
 
     def hacerSonido(self):
         return f"{self.nombre} con {self.edad} años de edad, esta diciendo {self.sonido}"
     
 class Pajaro(Animal):
-    def __init__(self, nombre, edad):
+    def __init__(self, nombre, edad, sonido):
         super().__init__(nombre, edad)
-        self.sonido = "¡PIO PIO!"
+        self.sonido = sonido
     
     def hacerSonido(self):
         return f"{self.nombre} con {self.edad} años de edad, esta diciendo {self.sonido}"
         
 
 class Gato(Animal):
-    def __init__(self, nombre, edad):
+    def __init__(self, nombre, edad, sonido):
         super().__init__(nombre, edad)
-        self.sonido = "¡Miauu!"
+        self.sonido = sonido
 
     def hacerSonido(self):
         return f"{self.nombre} con {self.edad} años de edad, esta diciendo {self.sonido}"
     
-animal1 = Perro("Tomy", 5)
+animal1 = Perro("Tomy", 5, "¡Guau Guau!")
 print(animal1.hacerSonido())
 
-animal2 = Gato("Kite", 2)
+animal2 = Gato("Kite", 2, "¡Miaaaaaaaau!")
 print(animal2.hacerSonido())
 
-animal3 = Pajaro("Piolin", 2)
+animal3 = Pajaro("Piolin", 2, "Pio Pio")
 print(animal3.hacerSonido())
